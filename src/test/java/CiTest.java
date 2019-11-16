631c210e79606a6641ce41cdf50fee6701eb3b0f
gwenn <gtreguier@gmail.com>
2019-11-16 15:45:19 +0100

    Debug CI build

--- src/test/java/CiTest.java
+++ src/test/java/CiTest.java
@@ -1,10 +1,14 @@
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 public class CiTest {
     @Test
     public void shouldLoadLibrary() {
+        System.out.print("PATH=");
+        System.out.println(System.getenv("PATH"));
+        System.out.print("LD_LIBRARY_PATH=");
+        System.out.println(System.getenv("LD_LIBRARY_PATH"));
+        System.out.print("java.library.path=");
+        System.out.println(System.getProperty("java.library.path"));
         System.loadLibrary("sqlite3");
     }
 }
